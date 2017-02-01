a7e2ca1f20f8a43375501f1312ecee555a5182d9
saylorsd <Hambone#25>
2017-01-31 23:41:52 -0500

    improved settings, better logging

--- settings-example.py
+++ settings-example.py
@@ -1,2 +1,10 @@
+#==============================================================================
+# SETTINGS
+#==============================================================================
+from led_matrix_ticker.fonts import CP437_FONT_ROTATED          # !! ADD YOUR FONT CHOICE HERE
+
 API_KEY = "YOUR_API_KEY_HERE!"
-END_TIME = "09:00"  # time for it to stop
+
+FONT = CP437_FONT_ROTATED                   # !! Change this to your font of choice (be sure to import it above!)
+LOG_FILE = "/home/pi/logs/bus-ticker.log"   # Where the log file will go.  Use Falsey value to log to stdout
+DURATION = 30                               # Number of minutes you want it to run for
--- ticker.py
+++ ticker.py
@@ -1,20 +1,37 @@
+#!/usr/bin/env python
+import sys
 import requests
 import xml.etree.ElementTree as ET
-from datetime import datetime as dt
+from datetime import datetime as dt, timedelta
 
-from led_matrix_ticker.led import LEDMatrixTicker as Ticker     # TODO: come up with better naming and organization
-from led_matrix_ticker.fonts import CP437_FONT_ROTATED          # !! ADD YOUR FONT CHOICE HERE
+from led_matrix_ticker.led import LEDMatrixTicker as Ticker  # TODO: come up with better naming and organization
 
-from settings import API_KEY, END_TIME
+from settings import API_KEY, LOG_FILE, DURATION, FONT, SPEED
 
-api_url = 'http://realtime.portauthority.org/bustime/api/v1/getpredictions'
-log_file = "bus-ticker.log"  # !! make empty if you don't want to keep a log
+# ==============================================================================
+# CONSTANTS
+# ==============================================================================
+API_URL = 'http://realtime.portauthority.org/bustime/api/v1/getpredictions'
+
+end_time = dt.now() + timedelta(minutes=DURATION)
+SPEED = (SPEED % 10) + 1
+
+# Fun Characters
+SMILE =     chr(0x01)
+HEART =     chr(0x03)
+DIAMOND =   chr(0x04)
+CLUB =      chr(0x05)
+SPADE =     chr(0x06)
+
+MALE = chr(0x0B)
+FEMALE = chr(0x0C)
+
+EIGTH_NOTE = chr(0x0D)
+DOUBLE_EIGTH_NOTE = chr(0x0E)
 
 
-end_time = dt.strptime(END_TIME, "%H:%M")
-end_time = end_time.replace(year=dt.now().year, month=dt.now().month, day=dt.now().day)
 
-def log(msg, file=log_file, mode='a'):
+def log(msg, file=LOG_FILE, mode='w', display=True):
     """
     Writes `msg` to log file at path `log_file`
 
@@ -22,21 +39,23 @@ def log(msg, file=log_file, mode='a'):
     :param file: str: path/to/log.txt
     :param mode: file open mode
     """
-    if log_file:
+    if file:
         with open(file, mode) as f:
             f.write("{}  ::  {}\n".format(dt.now().isoformat(), msg))
+    elif display:
+        print(msg)
 
 
-#==============================================================================
+# ==============================================================================
 # STOPS AND ROUTES
 # list of dicts that represent each prediction I'd like to see results on
 # the keys match the parameters on teh API
-#==============================================================================
+# ==============================================================================
 p_requests = [
     {
-        'stpid': 8161,      # stop id -- see readme for more details
-        'rt': 'P3',         # bus route name
-        'dir': 'INBOUND'    # direction: 'INBOUND' or 'OUTBOUND'
+        'stpid': 8161,  # stop id -- see readme for more details
+        'rt': 'P3',  # bus route name
+        'dir': 'INBOUND'  # direction: 'INBOUND' or 'OUTBOUND'
     },
     {
         'stpid': 18141,
@@ -45,46 +64,55 @@ p_requests = [
     }
 ]
 
-
-#==============================================================================
+# ==============================================================================
 # LED MATRIX SCROLLING TEXT TICKER
 # * in dictionary form for easy reading
-#==============================================================================
-ticker = Ticker(width=4, brightness=3, font=CP437_FONT_ROTATED,rotated=True)      # settings for my 1x4 matrix display
-scroll_times = 20       # number of times to scroll message before checking for new times
+# ==============================================================================
+ticker = Ticker(width=4, brightness=3, font=FONT, rotated=True)  # settings for my 1x4 matrix display
+scroll_times = 20  # number of times to scroll message before checking for new times
 
-
-#==============================================================================
+# ==============================================================================
 # SEND REQUESTS TO PORT AUTHORITY'S REALTIME API
-#==============================================================================
+# ==============================================================================
 # for each stop, route, direction combination provide above, find the
 # next arrival time and print it on the ticker `scroll_times` times
 # stop once it's past `end_time`
-#==============================================================================
+# ==============================================================================
 
 try:
-    message = ""
     while (dt.now() < end_time):
+        message = ""
         for request in p_requests:
-            request.update({'key': API_KEY})                    # add my api key to request parameters
-            response = requests.get(api_url, params=request)    # make request to API
-                                                                # see readme for example result
+            request.update({'key': API_KEY})  # add my api key to request parameters
+            response = requests.get(API_URL, params=request)  # make request to API
+            # see readme for example result
             if response.status_code == 200:
-                results = ET.fromstring(response.text)          # parse the XML response into an ElementTree
-                print(request['rt'])
+                results = ET.fromstring(response.text)  # parse the XML response into an ElementTree
+                log(request['rt'])
+
                 if not results.findall('error'):
-                    prediction = results.findall('prd')[0]      # per API: first prediction result == next arrival
+                    prediction = results.findall('prd')[0]  # per API: first prediction result == next arrival
+
                     if len(prediction):
                         arrival = dt.strptime(prediction.findall('prdtm')[0].text, "%Y%m%d %H:%M")
                         time = arrival - dt.now()
-                        message += "{}: {} ({:.0f}mins) ||".format(request['rt'], arrival.strftime('%H:%M'), (time.seconds/ 60))
+                        message += "{}: {} ({:.0f}mins) |".format(request['rt'], arrival.strftime('%H:%M'),
+                                                                  (time.seconds / 60))
 
-                else:
-                    print('[ERROR] - no predictions for {}'.format(request['rt']))
-                    log('[ERROR] - no predictions for {}'.format(request['rt']))  # log it
+                else:  # no predictions often means that a bus isn't coming in the next 30 minutes
+                    log('[ERROR] - no predictions for {}'.format(request['rt']))  #
 
-        print(message.rstrip('|'))              # rstrip to remove trailing '||'
-        ticker.scroll_message(message, speed=4, repeats=scroll_times)
+            else:
+                log('ERROR] - request error (code: {})'.format(response.status_code))
+                message = "API ERROR"
+
+        if not message:
+            message = "0 Buses Predicted Within Next 30 Minutes " + HEART
+
+        message = message.replace('|', DOUBLE_EIGTH_NOTE)
+        ticker.scroll_message(message, speed=SPEED, repeats=scroll_times)
+
     log('It ran without error!!')
+
 finally:
     ticker.clear_all()
