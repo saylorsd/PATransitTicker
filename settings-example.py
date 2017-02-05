06bafa7827c4bc9d9fd93c621aed99a3eabe0a3f
saylorsd <Hambone#25>
2017-02-05 12:22:07 -0500

    started implementing redis functionality

--- settings-example.py
+++ settings-example.py
@@ -5,13 +5,26 @@ from led_matrix_ticker.fonts import CP437_FONT_ROTATED          # !! ADD YOUR FO
 
 API_KEY = "YOUR_API_KEY_HERE!"              # TrueTime API key from Port Authority
 
-FONT = CP437_FONT_ROTATED                   # !! Change this to your font of choice (be sure to import it above!)
 LOG_FILE = "/home/pi/logs/bus-ticker.log"   # Where the log file will go.  Use Falsey value to log to stdout
+
+USE_REDIS = False                           # Set to true if publishing message to redis to be consumed by another ticker
+
+# TICKER SETTINGS
+# only used if USE_REDIS is False
+FONT = CP437_FONT_ROTATED                   # !! Change this to your font of choice (be sure to import it above!)
 DURATION = 30                               # Number of minutes you want it to run for
 WIDTH = 4                                   # Width of ticker (i.e. number of matrices)
 BRIGHTNESS = 1                              # LED brightness: 0-15
 SPEED = 5                                   # Message speed 1-10
 SCROLL_TIMES = 10                           # number of times to scroll message before checking for new times
+
+# REDIS SETTINGS
+# only used if USE_REDIS is True
+REDIS_DB = 0                                # probably don't need to change this
+REDIS_HOST = 'localhost'                    # probably don't need to change this
+REDIS_PORT = '6379'                         # probably don't need to change this
+
+REDIS_MSG_KEY = 'ticker_msg'                # the redis key to which you'll write the message
 
 
 # ==============================================================================
--- ticker.py
+++ ticker.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-import sys
-import requests
+import time
 import xml.etree.ElementTree as ET
+import requests
+import redis
+
 from datetime import datetime as dt, timedelta
 
 from led_matrix_ticker import LEDMatrixTicker as Ticker  # TODO: come up with better naming and organization
 
-from settings import API_KEY, LOG_FILE, DURATION, FONT, SPEED, PREDICTIONS, SCROLL_TIMES, BRIGHTNESS, WIDTH
+from settings import API_KEY, LOG_FILE, USE_REDIS, \
+    DURATION, FONT, SPEED, PREDICTIONS, SCROLL_TIMES, BRIGHTNESS, WIDTH, \
+    REDIS_DB, REDIS_HOST,  REDIS_MSG_KEY, REDIS_PORT
 
 # ==============================================================================
 # CONSTANTS
@@ -17,18 +21,26 @@ end_time = dt.now() + timedelta(minutes=DURATION)
 SPEED = (SPEED % 10) + 1
 
 # Fun Characters
-SMILE =     chr(0x01)
-_SMILE =    chr(0x02)
-HEART =     chr(0x03)
-DIAMOND =   chr(0x04)
-CLUB =      chr(0x05)
-SPADE =     chr(0x06)
+SMILE = chr(0x01)
+_SMILE = chr(0x02)
+HEART = chr(0x03)
+DIAMOND = chr(0x04)
+CLUB = chr(0x05)
+SPADE = chr(0x06)
 
 MALE = chr(0x0B)
 FEMALE = chr(0x0C)
 
 EIGTH_NOTE = chr(0x0D)
 DOUBLE_EIGTH_NOTE = chr(0x0E)
+
+SPLIT = DIAMOND
+
+if USE_REDIS:
+    redis_conn = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+else:
+    # instantiate LED Matrix Ticker controller
+    ticker = Ticker(width=WIDTH, brightness=BRIGHTNESS, font=FONT, rotated=True)  # settings for my 1x4 matrix display
 
 
 
@@ -47,51 +59,57 @@ def log(msg, file=LOG_FILE, mode='w', display=True):
         print(msg)
 
 
-# instantiate LED Matrix Ticker controller
-ticker = Ticker(width=WIDTH, brightness=BRIGHTNESS, font=FONT, rotated=True)  # settings for my 1x4 matrix display
+def get_message():
+    """
+    For each stop, route, direction combination provide above, find the
+    next arrival time make a nice string out of the information
+    :return: message to send to ticker
+    """
 
-# ==============================================================================
-# SEND REQUESTS TO PORT AUTHORITY'S REALTIME API
-# ==============================================================================
-# for each stop, route, direction combination provide above, find the
-# next arrival time and print it on the ticker `SCROLL_TIMES` times
-# stop once it's past `end_time`
-# ==============================================================================
+    message = ""
+    for request in PREDICTIONS:
+        request.update({'key': API_KEY})  # add my api key to request parameters
+        response = requests.get(API_URL, params=request)  # make request to API
+        # see readme for example result
+        if response.status_code == 200:
+            results = ET.fromstring(response.text)  # parse the XML response into an ElementTree
+            log(request['rt'])
 
+            if not results.findall('error'):
+                prediction = results.findall('prd')[0]  # per API: first prediction result == next arrival
+
+                if len(prediction):
+                    arrival = dt.strptime(prediction.findall('prdtm')[0].text, "%Y%m%d %H:%M")
+                    time = arrival - dt.now()
+                    message += "{}: {} ({:.0f}mins) | ".format(request['rt'], arrival.strftime('%H:%M'),
+                                                               (time.seconds / 60))
+
+            else:  # no predictions often means that a bus isn't coming in the next 30 minutes
+                log('[ERROR] - no predictions for {}'.format(request['rt']))
+
+        else:
+            log('ERROR] - request error (code: {})'.format(response.status_code))
+            message = "API ERROR"
+
+    if not message:
+        message = "0 Buses Predicted Within Next 30 Minutes " + SPLIT
+
+    message = message.replace('|', SPLIT)
+
+    return message
+
+# MAIN LOOP
 try:
     while (dt.now() < end_time):
-        message = ""
-        for request in PREDICTIONS:
-            request.update({'key': API_KEY})  # add my api key to request parameters
-            response = requests.get(API_URL, params=request)  # make request to API
-            # see readme for example result
-            if response.status_code == 200:
-                results = ET.fromstring(response.text)  # parse the XML response into an ElementTree
-                log(request['rt'])
-
-                if not results.findall('error'):
-                    prediction = results.findall('prd')[0]  # per API: first prediction result == next arrival
-
-                    if len(prediction):
-                        arrival = dt.strptime(prediction.findall('prdtm')[0].text, "%Y%m%d %H:%M")
-                        time = arrival - dt.now()
-                        message += "{}: {} ({:.0f}mins) | ".format(request['rt'], arrival.strftime('%H:%M'),
-                                                                  (time.seconds / 60))
-
-                else:  # no predictions often means that a bus isn't coming in the next 30 minutes
-                    log('[ERROR] - no predictions for {}'.format(request['rt']))
-
-            else:
-                log('ERROR] - request error (code: {})'.format(response.status_code))
-                message = "API ERROR"
-
-        if not message:
-            message = "0 Buses Predicted Within Next 30 Minutes " + DIAMOND
-
-        message = message.replace('|', DIAMOND)
-        ticker.scroll_message(message, speed=SPEED, repeats=SCROLL_TIMES)
-
-    log('It ran without error!!')
+        message = get_message()
+        if USE_REDIS:
+            test = redis_conn.set(REDIS_MSG_KEY, message)
+            time.sleep(60)
+        else:
+            ticker.scroll_message(message, speed=SPEED, repeats=SCROLL_TIMES)
 
 finally:
-    ticker.clear_all()
+    if not USE_REDIS:
+        ticker.clear_all()
+    else:
+        redis_conn.set(REDIS_MSG_KEY, "")
